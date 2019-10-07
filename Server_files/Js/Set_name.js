bcbc27bb182f85f1efc7203a175f3a2af2de3558
Pablo Collado <pcolladosoto@gmail.com>
2019-10-07 23:12:49 +0200

    Finally finished!

--- Server_files/Js/Set_name.js
+++ Server_files/Js/Set_name.js
@@ -1,4 +1,5 @@
 function set_name() {
   localStorage.name = document.getElementById("username").value;
+  //Como el sitio no está calificado como seguro al carecer de HTTPS algunos navegadores no permiten hacer este redireccionamiento. Nuestro novaegador de pruebas, Chromium, si que permite hacerlo.
   location.href = "../Index.html";
 }
