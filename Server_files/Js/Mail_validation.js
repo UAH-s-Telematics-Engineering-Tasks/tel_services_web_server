6473d7ef540b6d6f4eb7b637705fc71e353c1599
Pablo Collado <pcolladosoto@gmail.com>
2019-10-19 16:03:31 +0200

    Finished it!

--- Server_files/Js/Mail_validation.js
+++ Server_files/Js/Mail_validation.js
@@ -1,9 +1,7 @@
 // Source for validation rules: https://help.xmatters.com/ondemand/trial/valid_email_format.htm
 
 function validate_mail(input_box) {
-  //  alert("Hello!");
-  //alert(input_text.value.search(/^[a-z0-9]+([\._-][a-z0-9]+)*@[a-z0-9]+([\._-][a-z0-9]{3,})+$/i));
-  if (!input_box.value.search(/^[a-z0-9]+([\._-][a-z0-9]+)*@[a-z0-9]+([\._-][a-z0-9]{3,})+$/i)) {
+  if (!input_box.value.search(/^[a-z0-9]+([\._-][a-z0-9]+)*@[a-z0-9]+([\._-][a-z0-9]{2,})+$/i)) {
     input_box.setAttribute("class", "valid");
     return true;
   }
--- Server_files/Js/Set_name.js
+++ Server_files/Js/Set_name.js
@@ -1,7 +1,9 @@
-function set_name(input_text) {
-  localStorage.name = input_text.value;
+function set_name() {
+  localStorage.name = document.getElementById("username").value;
   //Como el sitio no está calificado como seguro al carecer de HTTPS algunos navegadores no permiten hacer este redireccionamiento. Nuestro novaegador de pruebas, Chromium, sí que permite hacerlo.
   //location.href = "../Index.html";
-  if (validate_mail(document.getElementById("mail").value))
+  if (validate_mail(document.getElementById("mail"))) {
     alert("Okay!");
+    location.href = "../Index.html";
+  }
 }
