4c1b9e426aa2de1f632ccb9df36f430610bc3da4
Pablo Collado <pcolladosoto@gmail.com>
2019-11-17 22:32:01 +0100

    Cleaned up the config script. Added an automation script for updating!

--- Server_config/Server_config_scipt.sh
+++ Server_config/Server_config_scipt.sh
@@ -1,27 +1,48 @@
 #!/bin/bash
 
-#sudo apt install apache2 #Apache2 is a daemon (hhtp) that listens for web requests and answers them!
+# Don't forget to add <serverIP> sroom.test.org to your /etc/hosts file for an easier access to the site!
 
-sudo mkdir /var/www/smart_room_root # Create a folder containing the webpage
+# Apache2 is a daemon (hhtp) that listens for web requests and answers them!
+# sudo apt install apache2
 
+# Create a folder containing the webpage
+sudo mkdir /var/www/smart_room_root
+
+# Get our server files from GitHub
 sudo git clone https://github.com/pcolladosoto/tel_services_web_server
 sudo git checkout pablo
 
-sudo  cp smart_room.conf /etc/apache2/sites-available/smart_room.conf # Copy the premade conf file to the appropriate location
+# Copy the premade .conf file to the appropriate location
+sudo  cp smart_room.conf /etc/apache2/sites-available/smart_room.conf
 
-sudo a2ensite smart_room.conf # Enable the virtual site configured in the above conf file
+# Enable the virtual site configured in the above conf file
+sudo a2ensite smart_room.conf
 
-#ls /etc/apache2/sites-enabled # Check if the site has been enabled correctly!
+# Disable apache's default site to be able to serve requests made to the server's IP instead of the hostname.
+# Requests made to sroom.test.org will carry that URL in an HTTP header field and Apache will be able to find
+# the virtual host. If the request is made to the server's IP Apache will just serve the default page unless
+# we disable it... If we do the only active page is smart_room so it'll be the only one we serve!
+sudo a2dissite 000-default.conf
 
-sudo service apache2 reload # Let the changes take effect!
+# Check whether the site has been enabled correctly!
+# ls /etc/apache2/sites-enabled
 
-sudo useradd --shell /usr/sbin/nologin web_admin # The no login shell refuses acces!
-sudo usermod -d /var/www/smart_room_root web_admin # Grant access to the web root!
-sudo usermod -a -G www-data web_admin # Add the user to the www-data group!
+# Let the changes take effect!
+sudo service apache2 reload
 
-sudo cp updated_shells /etc/shells # Declare nologin as a valid shell. Needed for some daemons...
+# The no login shell refuses acces!
+sudo useradd --shell /usr/sbin/nologin web_admin
 
-sudo chgrp www-data /var/www/smart_room_root/Server_files -R #Change the directory's group
-sudo chown web_admin /var/www/smart_room_root/Server_files -R # Change the directory's owner
+# Grant access to the web root!
+sudo usermod -d /var/www/smart_room_root web_admin
 
-#Don't forget to add <serverIP> sroom.test.org to your /etc/hosts file!
+# Add the user to the www-data group!
+sudo usermod -a -G www-data web_admin
+
+# Declare nologin as a valid shell. Needed for some daemons...
+sudo cp updated_shells /etc/shells
+
+# Change the directory's group
+sudo chgrp www-data /var/www/smart_room_root/Server_files -R
+# Change the directory's owner
+sudo chown web_admin /var/www/smart_room_root/Server_files -R
