da8a4143a6cb0ff09a2b633d53909f7293cf27f7
Pablo Collado <pcolladosoto@gmail.com>
2019-11-18 20:09:18 +0100

    Added JSON table data!

---
+++ Server_files/Js/Ajax_scripts/Tables_json.js
@@ -0,0 +1,41 @@
+var TABLE_HEADER = '<tbody><tr><th><button type="button" name="button" class="rounded show_hidden">Mostrar ocultos</button></th><th>Ciudad</th><th>Temperatura (&deg)</th><th>Humedad (%)</th><th>Ruido</th><th>Nivel de luz</th><th>Color de Iluminación</th></tr>';
+
+var SHOW_BUTTON = '<tr><td><button class="rounded no_back" type="button" name="button">Ocultar</button></td>';
+
+function get_n_create_table(trig) {
+  $.getJSON("../Ajax_files/Table_data.json", function() {
+    parse_data(data, trig);
+  });
+}
+
+function parse_data(resp, id) {
+  var table = TABLE_HEADER;
+
+  for (const ent of resp[0][id - 1])
+    table +=  SHOW_BUTTON +
+              '<td class="left">' +
+              ent.city +
+              '</td><td>' +
+              ent.temp +
+              '</td><td>' +
+              ent.hum +
+              '</td><td>' +
+              ent.noise +
+              '</td><td>' +
+              ent.light +
+              '</td><td>' +
+              ent.color +
+              '</td></tr>';
+
+    document.getElementById("tab" + id).innerHTML = table + '</tbody>';
+
+    style_me_up(id);
+}
+
+function style_me_up(n) {
+  $("#tab" + n + " tr:odd").css("background-color", "#00FF40");
+  $("#tab" + n + " tr:even").css("background-color", "#FA58F4");
+  $("#tab" + n + " tr:nth-child(1)").css("background-color", "#cccccc");
+  $("#tab" + n + " td:nth-child(1)").css("background-color", "#cccccc");
+  $("#tab" + n + " th:nth-child(1)").css("background-color", "#cccccc");
+}
