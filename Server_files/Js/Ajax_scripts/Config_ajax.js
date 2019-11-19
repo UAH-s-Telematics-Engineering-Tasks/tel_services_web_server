76b40676bfc5e127131412d90b064ce5f2858f42
Pablo Collado <pcolladosoto@gmail.com>
2019-11-19 08:49:49 +0100

    Testing config

--- Server_files/Js/Ajax_scripts/Config_ajax.js
+++ Server_files/Js/Ajax_scripts/Config_ajax.js
@@ -1,6 +1,5 @@
 function loadDoc(pais)
 {
-  // alert("Got to loadDoc()");
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200)
@@ -8,41 +7,13 @@ function loadDoc(pais)
       mostrar(pais, this.responseText);
     }
   };
-
-  // if (pais == "España")
-  // {
-  //   xhttp.open("GET", "../españa.txt", true);
-  // }
-  // else if (pais == "Francia")
-  // {
-  //   xhttp.open("GET", "../francia.txt", true);
-  // }
-  // else if (pais == "Italia")
-  // {
-  //   xhttp.open("GET", "../italia.txt", true);
-  // }
-  // else if (pais == "Inglaterra")
-  // {
-  //   xhttp.open("GET", "../inglaterra.txt", true);
-  // }
-  // else
-  // {
-  //   xhttp.open("GET", "../vacio.txt", true);
-  // }
-  //
-  // xhttp.send();
   xhttp.open("GET", "../Ajax_files/Cities_ajax.txt", true);
-  // alert("Hello");
   xhttp.send();
-  // alert("Reached the end of loadDoc()");
 
 }
 
 function mostrar(count, txt)
 {
-  alert("Hi from mostrar()");
-  for (ent of txt.split("\n"))
-    alert(ent);
   switch (count) {
     case "España":
       document.getElementById("city").value = txt.split("\n")[0];
