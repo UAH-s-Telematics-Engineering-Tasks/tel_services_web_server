17ecf63864d95d806b2ba573a385d5d38575b563
Pablo Collado <pcolladosoto@gmail.com>
2019-11-23 10:59:54 +0100

    Will it work?

--- Server_files/Js/Ajax_scripts/Tables_ajax.js
+++ Server_files/Js/Ajax_scripts/Tables_ajax.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  $("#pag1").show();
   get_n_create_table(1);
+  $("#pag1").show();
   $("#bot1").css("background-color", "yellow");
   $("#bot2").css("background-color", "white");
   $("#bot3").css("background-color", "white");
@@ -8,8 +8,6 @@ $(document).ready(function() {
   $("#sel1").css("background-color", "red");
 
   $("#bot1").click(function() {
-    $("#cont").children("div").hide();
-    $("#pag1").show();
     $("#bot1").css("background-color", "yellow");
     $("#bot2").css("background-color", "white");
     $("#bot3").css("background-color", "white");
@@ -36,8 +34,6 @@ $(document).ready(function() {
   });
 
   $("#bot3").click(function() {
-    $("#cont").children("div").hide();
-    $("#pag3").show();
     $("#bot3").css("background-color", "yellow");
     $("#bot1").css("background-color", "white");
     $("#bot2").css("background-color", "white");
@@ -50,8 +46,6 @@ $(document).ready(function() {
   });
 
   $("#bot4").click(function() {
-    $("#cont").children("div").hide();
-    $("#pag4").show();
     $("#bot4").css("background-color", "yellow");
     $("#bot1").css("background-color", "white");
     $("#bot2").css("background-color", "white");
@@ -64,12 +58,12 @@ $(document).ready(function() {
   });
 });
 
-function style_me_up(n) {
-  $("#tab" + n + " tr:odd").css("background-color", "#00FF40");
-  $("#tab" + n + " tr:even").css("background-color", "#FA58F4");
-  $("#tab" + n + " tr:nth-child(1)").css("background-color", "#cccccc");
-  $("#tab" + n + " td:nth-child(1)").css("background-color", "#cccccc");
-  $("#tab" + n + " th:nth-child(1)").css("background-color", "#cccccc");
+function style_me_up() {
+  $("#tab tr:odd").css("background-color", "#00FF40");
+  $("#tab tr:even").css("background-color", "#FA58F4");
+  $("#tab tr:nth-child(1)").css("background-color", "#cccccc");
+  $("#tab td:nth-child(1)").css("background-color", "#cccccc");
+  $("#tab th:nth-child(1)").css("background-color", "#cccccc");
   add_functionality();
 }
 
